ac297798fa58966e5e554327f462c97d96744637
Ingo Budde <ingo.budde@iem.fraunhofer.de>
2024-01-30 14:27:13 +0100

    fix examples branch

--- scan-linux.sh
+++ scan-linux.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 rm -rf build 
 mkdir -p build
+export CC="$(which clang)"
+export CXX="$(which clang++)"
 build-wrapper-linux-x86-64 --out-dir wrapper bash -c  "cmake -B build -S . && cmake --build build"
 sonar-scanner --define sonar.cfamily.build-wrapper-output=wrapper
